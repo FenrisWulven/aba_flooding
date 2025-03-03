3837cba339b3bebe8f41d58b5fa1db3e55c34257
FenrisWulven <ollie.elmgreen1@gmail.com>
2025-03-03 10:11:13 +0100

    Initial cookiecutter (based on MLOps course)

---
+++ src/aba_flooding/train.py
@@ -0,0 +1,10 @@
+from aba_flooding.model import Model
+from aba_flooding.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
